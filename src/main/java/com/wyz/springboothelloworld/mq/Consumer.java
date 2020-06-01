1649f4d54e55ddb602107e0ee34f1a00ffcbddec
wangyouzheng <qingdao2018>
2020-06-01 22:06:07 +0800

    active Consumer

--- src/main/java/com/wyz/springboothelloworld/mq/Consumer.java
+++ src/main/java/com/wyz/springboothelloworld/mq/Consumer.java
@@ -12,6 +12,14 @@ import javax.jms.*;
  */
 public class Consumer {
 	public static void main(String[] args) throws JMSException {
+		// manualReceive();
+		manualAcknowledge();
+	}
+
+	/**
+	 * 自动签收
+	 */
+	public static void autoReceive() throws JMSException {
 		// 去给连接工厂传入基础的连接参数。使用默认的账号密码 admin admin
 		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER, ActiveMQConnection.DEFAULT_PASSWORD, "tcp://127.0.0.1:61616");
 		// 创建连接
@@ -21,7 +29,7 @@ public class Consumer {
 		// 创建会话工厂
 		Session session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
 		// 创建队列
-		Destination destination = session.createQueue("my-queue");
+		Destination destination = session.createQueue("my-queue2");
 		// 创建消费者
 		MessageConsumer consumer = session.createConsumer(destination);
 		while (true) {
@@ -38,4 +46,62 @@ public class Consumer {
 		}
 	}
 
+	/**
+	 * 手动事务接收
+	 */
+	public static void manualReceive() throws JMSException {
+		// 去给连接工厂传入基础的连接参数。使用默认的账号密码 admin admin
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER, ActiveMQConnection.DEFAULT_PASSWORD, "tcp://127.0.0.1:61616");
+		// 创建连接
+		Connection connection = connectionFactory.createConnection();
+		// 启动连接
+		connection.start();
+		// 创建会话工厂 只是创建session   开启事务
+		Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
+		// 创建队列
+		Destination destination = session.createQueue("manal-queue2");
+		// 创建消费者
+		MessageConsumer consumer = session.createConsumer(destination);
+		while (true) {
+			System.out.println("开始调用了哈");
+			//
+			TextMessage receive = (TextMessage) consumer.receive();
+			if (receive != null) {
+				String text = receive.getText();
+				System.out.println("消费者获取到消息：" + text);
+			} else {
+				break;
+			}
+			session.commit(); // 不commit 在mq里面是看不到被消费了的
+			System.out.println("调用结束了哈");
+		}
+	}
+
+	public static void manualAcknowledge() throws JMSException {
+		// 去给连接工厂传入基础的连接参数。使用默认的账号密码 admin admin
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER, ActiveMQConnection.DEFAULT_PASSWORD, "tcp://127.0.0.1:61616");
+		// 创建连接
+		Connection connection = connectionFactory.createConnection();
+		// 启动连接
+		connection.start();
+		// 创建会话工厂 只是创建session
+		Session session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+		// 创建队列
+		Destination destination = session.createQueue("manal-queue2");
+		// 创建消费者
+		MessageConsumer consumer = session.createConsumer(destination);
+		while (true) {
+			System.out.println("开始调用了哈");
+			//
+			TextMessage receive = (TextMessage) consumer.receive();
+			if (receive != null) {
+				String text = receive.getText();
+				System.out.println("消费者获取到消息：" + text);
+			} else {
+				break;
+			}
+			// session.commit(); // 不commit 在mq里面是看不到被消费了的
+			System.out.println("调用结束了哈");
+		}
+	}
 }
--- src/main/java/com/wyz/springboothelloworld/mq/Producter.java
+++ src/main/java/com/wyz/springboothelloworld/mq/Producter.java
@@ -11,11 +11,21 @@ import javax.jms.*;
  * @Description:
  */
 public class Producter {
-	public static void main(String[] args) throws JMSException {
+	public static void main(String[] args) throws Exception {
+		Producter.transactionProducer();
+	}
+
+	static public void sendMsg(Session session, MessageProducer producer, String i) throws JMSException {
+		TextMessage textMessage = session.createTextMessage("hello activemq " + i);
+		producer.send(textMessage);
+	}
+
+	public static void producerAuto() throws Exception {
 		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER, ActiveMQConnection.DEFAULT_PASSWORD, "tcp://127.0.0.1:61616");
 		Connection connection = connectionFactory.createConnection();
 		connection.start();
 
+		// 是否以事务的方式进行提交， 是否要求自动签收/手动签收
 		Session session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
 		Destination destination = session.createQueue("my-queue");
 		MessageProducer producer = session.createProducer(destination);
@@ -29,8 +39,34 @@ public class Producter {
 		}
 		System.out.println("生产者发送完毕");
 	}
-	static public void sendMsg(Session session, MessageProducer producer, String i) throws JMSException {
-		TextMessage textMessage = session.createTextMessage("hello activemq " + i);
-		producer.send(textMessage);
+
+	/**
+	 * 持久化 + 手动提交
+	 *
+	 * @throws Exception
+	 */
+	public static void transactionProducer() throws Exception {
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER, ActiveMQConnection.DEFAULT_PASSWORD, "tcp://127.0.0.1:61616");
+		Connection connection = connectionFactory.createConnection();
+		connection.start();
+
+		// 事务提交 + 自动签收
+		// Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
+		// 事务提交 + 手动签收
+		Session session = connection.createSession(Boolean.TRUE, Session.CLIENT_ACKNOWLEDGE);
+		Destination destination = session.createQueue("manal-queue2");
+		MessageProducer producer = session.createProducer(destination);
+		// 不持久化
+		// producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+		// 设置持久化
+		producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("我是手动commit生产者" + i);
+			sendMsg(session, producer, "我是手动生产者 " + i);
+		}
+		// session.rollback();
+		session.commit();
+		System.out.println("生产者发送完毕");
+
 	}
 }
